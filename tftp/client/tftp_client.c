62276fae9d7a654dd52dcf0e26c2b47f4bfc11e8
Clem <98722223+CGomiero@users.noreply.github.com>
2022-11-05 12:18:45 -0700

    Update tftp_client.c

--- tftp/client/tftp_client.c
+++ tftp/client/tftp_client.c
@@ -3,92 +3,68 @@
 void run_client(struct tftp *my_tftp)
 {
     int n; // count bytes of stream sended or received
-    char ops[3];
+    int opc;
+    char opcode[3];
     char fileName[20];
     char stream[SSIZE];
+    FILE * fp = NULL;
 
-    /*
-        Needs to write codes again uisng tftp
-    */
+
     while(1)
     {
-    //     // receive a command 
-    //     scanf("%s" "%s", ops,fileName);
+        short blockNum = 0;
+        // receive input 
+        scanf("%s" "%s", opcode,fileName);
 
-    //     // get the opcode from the command
-	// 	if(ops[1] == 'r')
-	// 	{
-	// 		*(short*)stream = htons(RRQ);
-	// 	} 
-	// 	else if (ops[1] == 'w')
-	// 	{
-	// 		*(short*)stream = htons(WRQ);
-	// 	}
-    //     // get the filename from the command
-    //     p = stream + 2;
-    //     strcpy(p,fileName);
-    //     p += strlen(fileName) + 1; // 1 is for null
-    //     strcpy(p,MODE);
-    //     p += strlen(MODE) + 1;
+        // send request
+        n = my_tftp->send_req(my_tftp, opcode, fileName, MODE, stream);
+        // open file
+        opc = my_tftp->get_opc(my_tftp, stream);
+        if(opc == RRQ)
+        {
+            fp = fopen(fileName,"w");
+        }
+        else if(opc == WRQ)
+        {
+            fp = fopen(fileName,"r");
+            if(fp == NULL)
+            {
+                printf("%s: file not found error\n",my_tftp->progname);
+                exit(4);
+            }
+        }
 
-    //     n = p - stream;
-
-    //     // send a request
-    //     if(sendto(sockfd,stream,n,0,pserv_addr,servlen) != n)
-    //     {
-    //         printf("%s: sendto error on socket\n",progname);
-	// 		exit(3);
-    //     }
-
-    //     bzero(&stream,SSIZE);
+        bzero(&stream,SSIZE);
         
-    //     // receive a response
-    //     n =  recvfrom(sockfd,stream,SSIZE,0,NULL,NULL);
-    //     if (n < 0)
-    //     {
-    //         printf("%s: recvfrom error\n",progname);
-    //         exit(4);
-    //     }
-        
-    //    short blockNum = 1;
-    //    // check opcode
-    //    if(ntohs(*(short*) stream) == DATA) // receive DATA
-    //    {
-    //         // make a file with DATA
-	//         *(short*) (stream +2) = htons(blockNum);   
-    //         printf("Received Block #%d of data: %d byte(s)\n",blockNum,n);
-    //         FILE * fp = fopen(fileName,"w");
-    //         fwrite(stream+4,1,n-4,fp); // write 512 bytes
-    //         fclose(fp);
-    //    } 
-    //    else if(ntohs(*(short*) stream) == ACK) //receive ACK
-    //    {    
-    //         blockNum = ntohs(*(short*) (stream+2));  
-    //         printf("Received Ack #%d\n",blockNum);
-    //         bzero(&stream,SSIZE);
-    //         FILE *fp = fopen(fileName,"r");
-    //         if(fp == NULL)
-    //         {
-    //             printf("%s: file not found error\n",progname);
-    //             exit(4);
-    //         }
-    //         // send DATA
-    //         blockNum++;
-    //         *(short*) stream = htons(DATA);
-    //         *(short*) (stream+2) = htons(blockNum);
-    //         n = fread(stream+4,1,512,fp) + 4; // 4byte is for opcode and block number
-    //         if(sendto(sockfd,stream,n,0,pserv_addr,servlen) != n)
-    //         {
-    //             printf("%s: sendto error on socket\n",progname);
-    //             exit(3);
-    //         }   
-    //         else
-    //         {
-    //         printf("Sending Block #%d of data: %d byte(s)\n",blockNum,n);
-    //         }
-    //         fclose(fp); 
-    //    }
+        do
+        {
+            // get response
+            n = my_tftp->get_resp(my_tftp, stream);
+            // get opcode
+            opc = my_tftp->get_opc(my_tftp, stream);
+            if(opc == DATA)
+            {
+                // make a file with DATA
+                blockNum = ntohs(*(short*)(stream+2));
+                printf("Received Block #%d of data: %d byte(s)\n",blockNum,n);
+                fwrite(stream+4,1,n-4,fp); 
 
+                bzero(&stream,SSIZE); // clear stream
+                //send ACK
+                my_tftp->send_ack(my_tftp, stream, blockNum);
+                bzero(&stream,SSIZE); // clear stream
+            }
+            else if(opc == ACK)
+            {
+                blockNum = ntohs(*(short*)(stream+2));
+                printf("Received Ack #%d\n",blockNum);
+                bzero(&stream,SSIZE); // clear stream
+                n = my_tftp->send_dta(my_tftp,fp,stream,blockNum);
+                bzero(&stream,SSIZE); // clear stream
+            }
+        } while (n == 516);
+
+        fclose(fp);
     }
 }
 
