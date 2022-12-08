0cdc5435029d9ba8bfab157758ad661e1b1265f1
yoon2022 <114783362+yoon2022@users.noreply.github.com>
2022-12-07 23:43:55 -0800

    Final server

--- tftp/makefile/server/tftp_server.c
+++ tftp/makefile/server/tftp_server.c
@@ -3,33 +3,56 @@
 
 pthread_t tid;
 int retval;
+sigset_t sigSetMask;
+bool check = false; // for TIMEOUT TEST
 
 void* process_client(void* my_tftp);
 
 void run_server(struct tftp* serv_tftp)
 {
+    // block SIGALRM sent to the main thread
+    sigaddset(&sigSetMask, SIGALRM);
+    sigprocmask(SIG_BLOCK,&sigSetMask,NULL);
+    int oldCliPortNum = -1;
+    int currentCliPortNum = -1;
+
     while(1)
     {
+        // TIMEOUT CASE5,6 TEST - client doesn't receive ACK for the request and keeps sending RRQ or WRQ
+        // if(!check)
+        // {
+        //     check = true; // test once
+        //     sleep(8);
+        // }
+
         // receive request
         bzero(serv_tftp->inStream,SSIZE);
         serv_tftp->get_resp(serv_tftp,serv_tftp->inStream);  
+        // extract opcode from the request
+        serv_tftp->opcode = serv_tftp->get_opc(serv_tftp, serv_tftp->inStream);
+        // check clientPortNum to discard duplicated request;
+        oldCliPortNum = currentCliPortNum; 
+        currentCliPortNum = ntohs(serv_tftp->cli_addr.sin_port);
 
-        // build a new socket and port
-        struct tftp* new_tftp = tftp_init("./server");
-        new_tftp->build_serv(new_tftp,0);
-        // pass the client's ip and port
-        new_tftp->cli_addr.sin_addr = serv_tftp->cli_addr.sin_addr;
-        new_tftp->cli_addr.sin_port = serv_tftp->cli_addr.sin_port;
-        // pass request to new socket
-        bzero(new_tftp->inStream,SSIZE);
-        memcpy(new_tftp->inStream,serv_tftp->inStream,SSIZE);
-
-        // create a thread
-        retval = pthread_create(&tid, NULL, process_client, new_tftp);
-        if(retval != 0)
+        if((oldCliPortNum != currentCliPortNum) && (serv_tftp->opcode == RRQ || serv_tftp->opcode == WRQ))
         {
-            close(new_tftp->sockfd);
-            tftp_free(new_tftp);
+            // build a new socket and port
+            struct tftp* new_tftp = tftp_init("./server");
+            new_tftp->build_servThread(new_tftp);
+            // pass the client's ip and port
+            new_tftp->cli_addr.sin_addr = serv_tftp->cli_addr.sin_addr;
+            new_tftp->cli_addr.sin_port = serv_tftp->cli_addr.sin_port;
+            // pass request to new socket
+            bzero(new_tftp->inStream,SSIZE);
+            memcpy(new_tftp->inStream,serv_tftp->inStream,SSIZE);
+
+            // create a thread
+            retval = pthread_create(&tid, NULL, process_client, new_tftp);
+            if(retval != 0)
+            {
+                close(new_tftp->sockfd);
+                tftp_free(new_tftp);
+            }
         }
     }
 }
@@ -41,25 +64,31 @@ void* process_client(void* my_tftp)
     char client_addr[INET_ADDRSTRLEN];
     inet_ntop(AF_INET,&serv_tftp->cli_addr.sin_addr,client_addr,sizeof(client_addr));
     int client_port = ntohs(serv_tftp->cli_addr.sin_port);
-
-    // TIMEOUT CASE5,6 TEST - client doesn't receive ACK for the request and keeps sending RRQ or WRQ
-    // sleep(8);
+    // unblock SIGALRM sent to process client threads
+    pthread_sigmask(SIG_UNBLOCK,&sigSetMask,NULL);
 
     // parse the received request; extract filename, mode
     serv_tftp->parse_req(serv_tftp->fileName,serv_tftp->mode,serv_tftp->inStream);
-    // extract opcode from the request
-    serv_tftp->opcode = serv_tftp->get_opc(serv_tftp, serv_tftp->inStream);
-    
+    // check if mode is octet
+    if(strcmp(serv_tftp->mode,MODE) != 0) 
+    {
+        bzero(serv_tftp->outStream,SSIZE);
+        serv_tftp->send_err(serv_tftp,serv_tftp->outStream,4);
+        pthread_exit(PTHREAD_CANCELED);
+    }
     //check the filename to make sure it is not a filepath
     char * ptr;
     ptr = strchr(serv_tftp->fileName, '/');
-    
-      if(ptr != nullptr){
+
+    if(ptr != NULL)
+    {
         bzero(serv_tftp->outStream,SSIZE);  
         serv_tftp->send_err(serv_tftp,serv_tftp->outStream,3);
-        exit(0);
-      }
-      
+        pthread_exit(PTHREAD_CANCELED);
+    }
+    // extract opcode from the request
+    serv_tftp->opcode = serv_tftp->get_opc(serv_tftp, serv_tftp->inStream);
+    
     // process RRQ request
     if(serv_tftp->opcode == RRQ)
     {   
@@ -71,81 +100,26 @@ void* process_client(void* my_tftp)
             {
                 serv_tftp->fp = fopen(serv_tftp->fileName,"r");
                 serv_tftp->blockNum++;                                                      // set blockNum = 1 to send DATA #1
+                // send DATA 
+                bzero(serv_tftp->outStream,SSIZE); 
+                serv_tftp->snByte = serv_tftp->send_dta(serv_tftp,serv_tftp->fp,serv_tftp->outStream,serv_tftp->blockNum); 
+                alarm(TIMEOUT);
             }
             else                                                                            // server has no permission to read the file
             {
+                // send ERROR
                 bzero(serv_tftp->outStream,SSIZE);  
                 serv_tftp->send_err(serv_tftp,serv_tftp->outStream,2);
-                exit(0);
+                pthread_exit(PTHREAD_CANCELED);
             }
         }
         else                                                                                // server doesn't have the requested file 
         {
+            // send ERROR
             bzero(serv_tftp->outStream,SSIZE);
             serv_tftp->send_err(serv_tftp,serv_tftp->outStream,0);
-            exit(0);
+            pthread_exit(PTHREAD_CANCELED);
         }
-
-        do
-        {
-            // TIMEOUT CASE1 TEST - client doesn't recevie DATA and keeps sending ACK
-            // if(serv_tftp->blockNum == 30)
-            // {
-            //     sleep(8);
-            // }
-
-            // send DATA 
-            bzero(serv_tftp->outStream,SSIZE); 
-            serv_tftp->snByte = serv_tftp->send_dta(serv_tftp,serv_tftp->fp,serv_tftp->outStream,serv_tftp->blockNum); 
-            alarm(TIMEOUT);
-
-            while(1)
-            {
-                // wait response
-                bzero(serv_tftp->inStream,SSIZE);
-                serv_tftp->recByte = serv_tftp->get_resp(serv_tftp,serv_tftp->inStream);
-                if(serv_tftp->recByte < 0)
-                {
-                    if(errno = EINTR)
-                    {
-                        int dc = serv_tftp->retxDATA(serv_tftp);                                         // retransmit DATA when failed to receive the corresponding ACK
-                        if(dc == -1)                                                                     // client terminated
-                        {
-                            serv_tftp->snByte = 0;                                                       // escape the outer while loop
-                            break; 
-                        }
-                    }
-                    else
-                    {
-                        exit(0);
-                    }
-                }
-                else
-                {
-                    // extract opcode from the response 
-                    serv_tftp->opcode = serv_tftp->get_opc(serv_tftp, serv_tftp->inStream);
-                    if(serv_tftp->opcode == RRQ)                                                                // discard duplicated RRQs 
-                    {
-                        alarm(TIMEOUT);
-                        continue;
-                    }
-                    serv_tftp->timeout_reset(); 
-                }
-                // receive ACK 
-                if(serv_tftp->opcode == ACK)
-                {
-                    if(serv_tftp->blockNum > ntohs(*(short*)(serv_tftp->inStream+2)))                              // discard duplicated ACKs 
-                    {
-                        alarm(TIMEOUT);
-                        continue;
-                    }
-                    serv_tftp->blockNum = ntohs(*(short*)(serv_tftp->inStream+2));
-                    printf("[UDP/%s:%d] Received Ack #%d\n",client_addr,client_port,serv_tftp->blockNum);
-                    break;
-                }
-            }
-            serv_tftp->blockNum++;
-        } while (serv_tftp->snByte == 516);                                                                        // terminate when all contents of the file has sended
     }
     // process WRQ request
     else if(serv_tftp->opcode == WRQ)
@@ -154,9 +128,9 @@ void* process_client(void* my_tftp)
         if(access(serv_tftp->fileName,F_OK) == 0)                                                           // check if the file exists
         {
             bzero(serv_tftp->outStream,SSIZE); 
-            // send ERR
+            // send ERROR
             serv_tftp->send_err(serv_tftp,serv_tftp->outStream,1);         
-            exit(0);
+            pthread_exit(PTHREAD_CANCELED);
         }
         else
         {
@@ -164,73 +138,149 @@ void* process_client(void* my_tftp)
             // send ACK for the WRQ request
             bzero(serv_tftp->outStream,SSIZE); 
             serv_tftp->send_ack(serv_tftp, serv_tftp->outStream, serv_tftp->blockNum);    
-            alarm(2);
+            alarm(TIMEOUT);
         }
+    }
 
-        do
+    while(1)
+    {
+        // wait response
+        bzero(serv_tftp->inStream,SSIZE);
+        serv_tftp->recByte = serv_tftp->get_resp(serv_tftp, serv_tftp->inStream);
+        // data loss control
+        if(serv_tftp->recByte < 0)
         {
-            while(1)
+            if(errno == EINTR)
             {
-                // wait response
-                bzero(serv_tftp->inStream,SSIZE); 
-                serv_tftp->recByte = serv_tftp->get_resp(serv_tftp, serv_tftp->inStream);
-                if(serv_tftp->recByte < 0)
-                {
-                    if(errno = EINTR)
-                    {
-                        int dc = serv_tftp->retxACK(serv_tftp);                                             // retransmit ACK when failed to receive the corresponding DATA
-                        if(dc == -1)                                                                        // client terminated
-                        {
-                            serv_tftp->recByte = 0;                                                         // set 0 to escape the outer while loop
-                            break; 
-                        }
-                    }
-                    else
-                    {
-                        exit(0);
-                    }
-                }
-                else
-                {
-                    serv_tftp->opcode = serv_tftp->get_opc(serv_tftp, serv_tftp->inStream);
-                    if(serv_tftp->opcode == WRQ)                                                               // discard duplicated WRQ
-                    {
-                        alarm(TIMEOUT);
-                        continue;
-                    } 
-                    serv_tftp->timeout_reset();
-                }
-
+                int dc = 0;
                 if(serv_tftp->opcode == DATA)
                 {
-                    if(serv_tftp->blockNum > ntohs(*(short*)(serv_tftp->inStream+2)))                            // discard duplicated DATA 
-                    {
-                        alarm(TIMEOUT);
-                        continue;
-                    }
-                    serv_tftp->blockNum = ntohs(*(short*)(serv_tftp->inStream+2));
-                    printf("[UDP/%s:%d] Received Block #%d of data: %d byte(s)\n",client_addr,client_port,serv_tftp->blockNum,(serv_tftp->recByte-4));
-                    fwrite(serv_tftp->inStream+4,1,serv_tftp->recByte-4,serv_tftp->fp); 
+                    dc = serv_tftp->retxDATA(serv_tftp);  
                 }
-
-                // TIMEOUT CASE3 TEST  - client doesn't recevie ACK and keeps sending DATA
-                // if(serv_tftp->blockNum == 30)
-                // {
-                //     sleep(8);
-                // }
-                
-                // send ACK
-                bzero(serv_tftp->outStream,SSIZE); 
-                serv_tftp->send_ack(serv_tftp, serv_tftp->outStream, serv_tftp->blockNum);    
-                if(serv_tftp->recByte == 516)                                                                       // don't set alarm after sending ACK when all packets are received
+                else if(serv_tftp->opcode == ACK)
                 {
-                    alarm(2);
+                    dc = serv_tftp->retxACK(serv_tftp);
+                }
+                if(dc == -1)                                                                     // client terminated
+                {
+                    break; 
+                }
+                continue;
+            }
+            else
+            {
+                pthread_exit(PTHREAD_CANCELED);
+            }
+        }
+        else
+        {
+            serv_tftp->timeout_reset();
+        }
+
+        // extract opcode from the response 
+        serv_tftp->opcode = serv_tftp->get_opc(serv_tftp, serv_tftp->inStream);
+
+            // check opcode of response
+        if(serv_tftp->opcode == DATA)
+        {
+            // next DATA corresponding sent ACK
+            if((serv_tftp->blockNum+1) == ntohs(*(short*)(serv_tftp->inStream+2)))
+            {
+                serv_tftp->blockNum = ntohs(*(short*)(serv_tftp->inStream+2));
+                printf("[UDP/%s:%d] Received Block #%d of data: %d byte(s)\n",client_addr,client_port,serv_tftp->blockNum,(serv_tftp->recByte-4));
+                fwrite(serv_tftp->inStream+4,1,serv_tftp->recByte-4,serv_tftp->fp);                                 // make a file with received DATA 
+            }
+            // discard duplicated DATA: send ACK
+            else if(serv_tftp->blockNum > ntohs(*(short*)(serv_tftp->inStream+2)))
+            {
+                printf("[UDP/%s:%d] Received Block #%d of data: %d byte(s)\n",client_addr,client_port,serv_tftp->blockNum,(serv_tftp->recByte-4));
+                continue;
+            }
+            // send ACK
+            // TIMEOUT CASE3 TEST  - client doesn't recevie ACK and keeps sending DATA
+            // if(serv_tftp->blockNum == 5 && !check)
+            // {
+            //     check = true; // to prevent to sleep as much as times that it receives duplicated DATA
+            //     sleep(8);
+            // }
+            bzero(serv_tftp->outStream,SSIZE); 
+            serv_tftp->snByte = serv_tftp->send_ack(serv_tftp, serv_tftp->outStream, serv_tftp->blockNum);
+            if(serv_tftp->recByte == 516)
+            {
+                alarm(TIMEOUT);
+            }
+            // last DATA to receive
+            else
+            {
+                break;
+            }
+        }
+        else if (serv_tftp->opcode == ACK)
+        {
+            // ACK to corresponding sent DATA
+            if(serv_tftp->blockNum == ntohs(*(short*)(serv_tftp->inStream+2)))
+            {
+                printf("[UDP/%s:%d] Received Ack #%d\n",client_addr,client_port,serv_tftp->blockNum);
+            }
+            // discard duplicated ACK
+            else if(serv_tftp->blockNum > ntohs(*(short*)(serv_tftp->inStream+2)))
+            {
+                alarm(TIMEOUT);
+                continue;
+            }
+            // send next DATA
+            // TIMEOUT CASE1 TEST - client doesn't recevie DATA and keeps sending ACK
+            // if(serv_tftp->blockNum == 5)
+            // {
+            //     sleep(8);
+            // }
+            serv_tftp->blockNum++;
+            bzero(serv_tftp->outStream,SSIZE);   
+            serv_tftp->snByte = serv_tftp->send_dta(serv_tftp,serv_tftp->fp,serv_tftp->outStream,serv_tftp->blockNum);
+            if(serv_tftp->snByte == 516)
+            {
+                alarm(TIMEOUT);
+            }
+            // last DATA to send
+            else
+            {
+                alarm(TIMEOUT);
+                // wait response
+                bzero(serv_tftp->inStream,SSIZE);
+                serv_tftp->recByte = serv_tftp->get_resp(serv_tftp, serv_tftp->inStream);
+                // extract opcode from the request
+                serv_tftp->opcode = serv_tftp->get_opc(serv_tftp, serv_tftp->inStream);
+                // ACK to corresponding sent DATA
+                if(serv_tftp->blockNum == ntohs(*(short*)(serv_tftp->inStream+2)))
+                {
+                    printf("[UDP/%s:%d] Received Ack #%d\n",client_addr,client_port,serv_tftp->blockNum);
+                }
+                // discard duplicated ACK
+                else if(serv_tftp->blockNum > ntohs(*(short*)(serv_tftp->inStream+2)))
+                {
+                    alarm(TIMEOUT);
                 }
                 break;
             }
-            serv_tftp->blockNum++;
-        } while (serv_tftp->recByte == 516);
-    }  
+        }
+        else if (serv_tftp->opcode == ERROR)
+        {
+            char errMsg[50];
+            short errCode = ntohs(*(short*)(serv_tftp->inStream+2));
+            strcpy(errMsg,(serv_tftp->inStream+4));
+            printf("Received ERROR #%d: %s\n",errCode,errMsg);
+            if(serv_tftp->fp != NULL)
+            {
+                fclose(serv_tftp->fp);
+                serv_tftp->fp = NULL;
+            }
+            if(errCode == 0 || errCode == 2)                                                                 // not found or no permission
+            {
+                remove(serv_tftp->fileName);                                                                  // remove the file created to read from server
+            }
+            exit(0);
+        }
+    }
 
     if(serv_tftp->fp != NULL)
     {
@@ -240,8 +290,11 @@ void* process_client(void* my_tftp)
 
     // terminate the thread
     close(serv_tftp->sockfd);
-    tftp_free(serv_tftp);                                                                                             
+    tftp_free(serv_tftp);    
+    printf("Finished!\n");
 }
+
+    //##########################################################################
 
 int main(int argc, char *argv[])
 {
